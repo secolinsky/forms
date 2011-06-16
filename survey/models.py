b220dfb2cb49a4c40e9ea37b9c591b5ecc1910f7
Maurice Ticas <maurice.ticas@gmail.com>
2011-06-16 11:44:11 -0700

    oswestry survey complete

--- survey/models.py
+++ survey/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.forms import ModelForm
+from django import forms
 
 class Section(models.Model):
     title = models.CharField(max_length=200)
@@ -11,3 +11,30 @@ class Choice(models.Model):
     choice = models.CharField(max_length=200)
     def __unicode__(self):
         return self.choice
+
+class Age(forms.Form):
+    AGE_CHOICE = (
+        ('lt','less than 60'),
+        ('gt','greater than 60')
+        )
+    AGE = forms.ChoiceField(choices=AGE_CHOICE)    
+   
+class Procedure(forms.Form):
+    PROCEDURE_CHOICES = (
+        ('Arthoplasty', 'Arthoplasty'),
+        ('Decompression with 1 level fusion', 'Decompression with 1 level fusion'),
+        ('Decompression with 2 level fusion', 'Decompression with 2 level fusion'),
+        ('Decompression 2 or more levels of fusion', 'Decompression 2 or more levels of fusion'),
+        )
+    PROCEDURE = forms.ChoiceField(choices=PROCEDURE_CHOICES)
+
+class Status(forms.Form):
+    PROCEDURE_CHOICES = (
+        ('Preoperative', 'Preoperative'),
+        ('4 weeks postop', '4 weeks postop'),
+        ('6 weeks postop', 'Decompression with 2 level fusion'),
+        ('12 weeks postop', 'Decompression 2 or more levels of fusion'),
+        ('6 months postop', 'Decompression 2 or more levels of fusion'),
+        ('12 months postop', 'Decompression 2 or more levels of fusion'),                
+        )
+    STATUS = forms.ChoiceField(choices=PROCEDURE_CHOICES)
--- survey/views.py
+++ survey/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
-from survey.models import Section
+from survey.models import Section, Age, Procedure, Status
 
 def index(request):
     sections=Section.objects.all()
-    return render_to_response('survey/index.html',{'sections': sections},
+    age=Age()
+    procedures=Procedure()
+    status=Status()
+    return render_to_response('survey/index.html',
+                              {'sections': sections,'age': age,'procedure':procedures,'status':status},
                               context_instance=RequestContext(request))
 
 def thank_you(request):    
@@ -14,5 +18,5 @@ def thank_you(request):
     for s in sections:
         if s != "csrfmiddlewaretoken":
             selections.append(sections[s])
+    age=Age(request.POST)
     return render_to_response('survey/thank_you.html',{'data': selections})
-    
